2a1a4819e8e557d2bf6d97766354540fb630abaa
Bruno-A-Z <zanateli@hotmail.com>
2025-11-15 14:26:54 -0300

    Feat: FOi criado a pagina de candidatos

---
+++ BPM-Connect/src/routes/Candidatos.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../components/layout/Layout";
+
+export function Candidatos() {
+  return (
+    <Layout>
+      <div className="max-w-4xl mx-auto py-8 text-center">
+        <h1 className="text-3xl font-bold mb-4">Candidatos</h1>
+        <p className="text-xl opacity-80">Página em desenvolvimento</p>
+      </div>
+    </Layout>
+  );
+}
